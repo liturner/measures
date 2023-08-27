e4166558f1f144b2c7f3ef59c7251549d34d7fea
liturner <turnertech@outlook.de>
2023-08-27 07:40:19 +0200

    Site checkin for project Measures

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"de.turnertech.measures","l":"de.turnertech.measures"}];updateSearchResults();
---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.turnertech.measures","l":"Measure"},{"p":"de.turnertech.measures","l":"Unit"},{"p":"de.turnertech.measures","l":"UnitConverter"}];updateSearchResults();
